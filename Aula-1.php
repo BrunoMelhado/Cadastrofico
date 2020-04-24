01be84749a934c44005c05fc126289f827cd4b97
Bruno Melhado <brunovmelhado@gmail.com>
2020-04-23 21:24:38 -0300

    Tabela HTML com dados estaticos

---
+++ Aula-1.php
@@ -0,0 +1,65 @@
+
+<html>
+
+    <head></head>
+
+    <body>
+
+        <table border=1>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>SEXO</th>
+            <th>CFP</th>
+            <th>Telefone</th>
+            
+            
+                <tr>
+                    <td>1</td>
+                    <td>Bruno</td>
+                    <td>bruno@gmail.com</td>
+                    <td>M</td>
+                    <td>45496007801</td>
+                    <td>999707407</td>
+
+                </tr>
+                  
+                <tr>
+                    
+                    <td>2</td>
+                    <td>Evandro</td>
+                    <td>Evandro@gmail.com</td>
+                    <td>M</td>
+                    <td>123456789</td>
+                    <td>123456789789</td>
+
+                </tr>
+
+
+
+
+
+
+
+
+        </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </body>
+
+
+
+
+</html>
