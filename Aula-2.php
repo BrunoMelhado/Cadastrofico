05e21e5dd3bbb7cf5581e1783a21c399c9b6ec38
Bruno Melhado <brunovmelhado@gmail.com>
2020-04-23 21:28:48 -0300

    Tabela HTML com dados estaticos em variaveis de PHP

---
+++ Aula-2.php
@@ -0,0 +1,78 @@
+<?php
+$nome = "Bruno Melhado";
+$id = "1";
+$email = "Bruno@gmail.com";
+$sexo = "M";
+$cpf = "45496007801";
+$telefone = "19999707407";
+$nome2 = "Evandro tomazine";
+$id2 = "2";
+$email2 = "evandro@gmail.com";
+$sexo2 = "M";
+$cpf2 = "45491237801";
+$telefone2 = "19999123407";
+?>
+<html>
+
+    <head></head>
+
+    <body>
+
+        <table border=1>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>SEXO</th>
+            <th>CFP</th>
+            <th>Telefone</th>
+            
+            
+                <tr>
+                    <td><?= $id ?></td>
+                    <td><?= $nome ?></td>
+                    <td><?= $email ?></td>
+                    <td><?= $sexo ?></td>
+                    <td><?= $cpf ?></td>
+                    <td><?= $telefone ?></td>
+
+                </tr>
+                  
+                <tr>
+                    
+                    <td><?= $id2 ?></td>
+                    <td><?= $nome2 ?></td>
+                    <td><?= $email2 ?></td>
+                    <td><?= $sexo2 ?></td>
+                    <td><?= $cpf2 ?></td>
+                    <td><?= $telefone2 ?></td>
+
+                </tr>
+
+
+
+
+
+
+
+
+        </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </body>
+
+
+
+
+</html>
